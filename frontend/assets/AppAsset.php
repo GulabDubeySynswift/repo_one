652e6fb204aecacd141fa38c2e40811cc64cfb02
Gulab Dubey <gulab@synswift.com>
2023-09-19 17:16:34 +0530

    second push

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -9,7 +9,7 @@ use yii\web\YiiAsset;
  * Main frontend application asset bundle.
  */ 
 class AppAsset extends AssetBundle
-{ 
+{  
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
